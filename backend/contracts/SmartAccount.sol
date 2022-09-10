943b5b4b1566fe5ff3e078de4fa69c4e607b5b4b
timolegros <timothee@ualberta.ca>
2022-09-10 17:04:00 -0600

    initial contracts push

---
+++ backend/contracts/SmartAccount.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+
+// import "hardhat/console.sol";
+
+contract SmartAccount is Initializable {
+    address payable public owner;
+    address payable public charity;
+    address public savingPercent;
+    address public sponsor;
+    address public savingsLocked;
+
+    uint256 public checkingAmt;
+    uint256 public savingsAmt;
+
+    uint256 public betCounter = 0;
+
+    enum BetState {INCOMPLETE, WIN, LOSE}
+
+    struct Bet {
+        uint256 amount;
+        BetState state;
+        string description;
+    }
+
+    mapping(uint => Bet) public bets;
+
+    event Withdrawal(uint amount, uint when);
+
+    function initialize(address payable owner_, uint256 savingPercent_, address sponsor_, address payable charity_) public initializer {
+        owner = owner_;
+        savingPercent = savingPercent_;
+        sponsor = sponsor_;
+        charity = charity_;
+    }
+
+    function withdraw(bool checkingAccount, uint256 amount) external {
+        require(msg.sender == owner, "You aren't the owner");
+
+        if (checkingAccount) {
+            require(checkingAmt >= amount, "Insufficient funds");
+            emit Withdrawal(amount, block.timestamp);
+            checkingAmt -= amount;
+            (bool sent, bytes memory data) = owner.call{value: amount}("");
+            require(sent, "Failed to send Ether");
+        } else {
+            require(!savingsLocked, "Savings is locked!");
+            require(savingsAmt >= amount, "Insufficient funds");
+            emit Withdrawal(amount, block.timestamp);
+            savingsAmt -= amount;
+            (bool sent, bytes memory data) = owner.call{value: amount}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+
+    function placeBet(uint256 amount, string description) {
+        require(msg.sender == owner, "You aren't the owner");
+        require(checkingAmt >= amount, "Insufficient funds");
+        betCounter += 1;
+        bets[betCounter] = Bet(amount, BetState.INCOMPLETE, description);
+        checkingAmt -= amount;
+    }
+
+    // Sponsor Functions
+
+    function setSavingsLock(bool lock) external {
+        require(msg.sender == sponsor, "You aren't the sponsor");
+        savingsLocked = lock;
+    }
+
+    function reviewBet(uint256 betIndex, BetState state) {
+        require(msg.sender == sponsor, "You aren't the sponsor");
+        require(bets[betIndex] != address(0), "Bet does not exist");
+        require(bets[betIndex].state == BetState.INCOMPLETE, "Bet has already been reviewed");
+        require(state == BetState.LOSE || state == BetState.WIN, "Bet state must be either win or lose");
+
+        if (state == BetState.WIN) {
+            // update bet state and return bet funds to checking account
+            bets[betIndex].state = BetState.WIN;
+            checkingAmt += bets[betIndex].amount;
+        } else {
+            bets[betIndex].state = BetState.LOSE;
+            (bool sent, bytes memory data) = charity.call{value: bets[betIndex].amount}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+
+    receive() payable external {
+        savingsAmt += (msg.value * savingPercent) / 100;
+        checkingAmt += (msg.value * (100 - savingsPercent)) / 100;
+    }
+
+    fallback() payable external {
+        savingsAmt += (msg.value * savingPercent) / 100;
+        checkingAmt += (msg.value * (100 - savingsPercent)) / 100;
+    }
+}
---
+++ backend/contracts/SmartAccountFactory.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/proxy/Clones.sol';
+import './SmartAccount.sol';
+
+contract SmartAccountFactory {
+    using Clones for address;
+
+    event AccountCreated(uint256 accountIndex, address accountAddress);
+
+    uint256 accountIndex = 0;
+    address accountImpl;
+    address charity;
+    mapping (address => address) public accounts;
+
+    constructor(address accountImpl_, address charity_) {
+        require(accountImpl_ != address(0), "Must be a valid address");
+        accountImpl = accountImpl_;
+        charity = charity_;
+    }
+
+    function createAccount(uint256 savingPercent, address sponsor) external {
+        require(accounts[msg.sender] == address(0), "You already have an account");
+        require(msg.sender != sponsor, "You can not be your own sponsor");
+        require(savingPercent <= 100, "Saving percent must be within [0, 100]");
+
+        accountIndex += 1;
+        bytes32 hash = keccak256(
+            abi.encodePacked(msg.sender, accountIndex)
+        );
+
+        address newAccount = accountImpl.cloneDeterministic(hash);
+        emit AccountCreated(accountIndex, newAccount);
+
+        SmartAccount(newAccount).initialize(msg.sender, savingPercent, sponsor, charity);
+    }
+}
