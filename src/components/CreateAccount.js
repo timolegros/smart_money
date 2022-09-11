190085a18e1b9cd2c55a38a7b7116f3715356a8d
timolegros <timothee@ualberta.ca>
2022-09-10 19:46:10 -0600

    percent value

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,12 +1,17 @@
 import Form from 'react-bootstrap/Form';
+import {useState} from "react";
 
 const CreateAccount = () => {
+    const [savingPercentValue, setSavingPercentValue] = useState();
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setSavingPercentValue(e.target.value);
+    }
     return (
         <div>
             <Form>
                 <Form.Label >What percentage of your funds should be in your savings account?</Form.Label>
-                <Form.Range id="fund-percent-slider" />
-                {console.log(document.getElementById("fund-percent-slider").value)}
+                <Form.Range id="fund-percent-slider" onChange={handleChange} min="0" max="100" step="1"/>
                 <b></b>
             </Form>
         </div>
