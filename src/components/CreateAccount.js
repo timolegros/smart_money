dd2cad64929e10e80de2f1341b96042a1ff224d7
Sukanta Saha <sukanta@ualberta.ca>
2022-09-10 20:12:21 -0600

    adad

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,4 +1,5 @@
 import Form from 'react-bootstrap/Form';
+import {Button} from 'react-bootstrap'
 import {useState} from "react";
 
 const CreateAccount = () => {
@@ -10,9 +11,21 @@ const CreateAccount = () => {
     return (
         <div>
             <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Sponsor's METAMASK Address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter address" />
+                    <Form.Text className="text-muted">
+                        We'll never share this address with anyone else
+                    </Form.Text>
+                </Form.Group>
+
                 <Form.Label >What percentage of your funds should be in your savings account?</Form.Label>
-                <Form.Range id="fund-percent-slider" onChange={handleChange} min="0" max="100" step="1" defaultValue="20"/>
+                <Form.Range id="fund-percent-slider" onChange={handleChange} min="0" max="100" step="1"/>
                 <b></b>
+
+                <Button variant="primary" type="submit" style={{marginTop: "1em", marginBottom: "5em"}}>
+                    Submit
+                </Button>
             </Form>
         </div>
      );
